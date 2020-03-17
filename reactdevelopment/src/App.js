e7b2229837c6f6d9eddc95e3bbe457e8fb52edc6
pavan-1357-kumar <62290430+pavan-1357-kumar@users.noreply.github.com>
2020-03-17 18:24:53 +0530

    Update App.js
    Developbranch

--- reactdevelopment/src/App.js
+++ reactdevelopment/src/App.js
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         {/* i have commented here */}
+        {/* develop branch */}
         <a
           className="App-link"
           href="https://reactjs.org"
