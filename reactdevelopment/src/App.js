ac8df11c10858d82cbaaeee00f7fa1767c235276
princechouhan04 <62290682+princechouhan04@users.noreply.github.com>
2020-03-17 18:21:57 +0530

    Update App.js
    added a comment

--- reactdevelopment/src/App.js
+++ reactdevelopment/src/App.js
@@ -10,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        {/* comment by prince */}
         <a
           className="App-link"
           href="https://reactjs.org"
