91417f7f01be101491628e8d5d22f2487bc666f6
pavan-1357-kumar <62290430+pavan-1357-kumar@users.noreply.github.com>
2020-03-17 18:04:20 +0530

    Update App.js
    Added some comments

--- reactdevelopment/src/App.js
+++ reactdevelopment/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        {/* commit */}
+        {/* i have commented here */}
         <a
           className="App-link"
           href="https://reactjs.org"
