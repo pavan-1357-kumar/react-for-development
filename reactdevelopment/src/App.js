8324591a4329f69d6508e288ff8f61a9be347b8b
pavan-1357-kumar <62290430+pavan-1357-kumar@users.noreply.github.com>
2020-03-17 19:27:40 +0530

    Update App.js
    this is the new comment from pavan

--- reactdevelopment/src/App.js
+++ reactdevelopment/src/App.js
@@ -22,7 +22,7 @@ function App() {
 
         {/* comment by prince */}
 
-
+{/* this is the new comment from pavan */}
         <a
           className="App-link"
           href="https://reactjs.org"
