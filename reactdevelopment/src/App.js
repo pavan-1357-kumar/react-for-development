f7af1feae8d9843b212a00dc306efc6c965863ac
princechouhan04 <62290682+princechouhan04@users.noreply.github.com>
2020-03-17 19:18:30 +0530

    Update App.js

--- reactdevelopment/src/App.js
+++ reactdevelopment/src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* This is new comment */}
 {/* commit to prince branch */}
         {/* comment by prince */}
         <a
