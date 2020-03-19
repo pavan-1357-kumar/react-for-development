b31fa059955b8f44f7a3fac14b1e4976b0c72a64
deepakthandu <thandu.deepak@gmail.com>
2020-03-19 11:59:08 +0530

    Update App.js
    changes done by deepak

--- reactdevelopment/src/App.js
+++ reactdevelopment/src/App.js
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 
+{/* this comment is by deepak thandu */}
         {/* This is new comment */}
 {/* commit to prince branch */}
 
