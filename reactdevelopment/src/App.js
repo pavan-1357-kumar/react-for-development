ff847921d8e86247a4b57ffdd48257ffa7f21ec7
Harisree98 <52851667+Harisree98@users.noreply.github.com>
2020-03-19 15:17:15 +0530

    Update App.js
    hari changed

--- reactdevelopment/src/App.js
+++ reactdevelopment/src/App.js
@@ -12,6 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* helllo */}
         {/* harisreeeeeeeeeeeeeeeeeeeeeeeeeeee */}
        
         {/* this is third by lalitha */}
